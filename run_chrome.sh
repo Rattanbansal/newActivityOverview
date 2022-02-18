462e9b9b8d4328cb297f23a2c81f1311de11a143
Rattan Bansal <rattan.intersoft@gmail.com>
2022-02-18 12:47:16 +0530

    Integration of docker

---
+++ run_chrome.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -xe
+
+docker run -t --rm -v /home/intersoft-admin/Rattan/Katalon/newActivityOverview:/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
