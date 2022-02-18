45933c60c1291c89f2b6bd747024f916288ac855
Rattanbansal <rattan.intersoft@gmail.com>
2022-02-18 23:55:52 +0530

    Update Path for the root directory

--- run_chrome.sh
+++ run_chrome.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-docker run -t --rm -v /home/intersoft-admin/Rattan/Katalon/newActivityOverview:/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
+docker run -t --rm -v /root/project:/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
