51b468b8fc360ab94cb9157254b3548882591735
Rattan Bansal <rattan.intersoft@gmail.com>
2022-02-19 18:47:11 +0530

    Add Multiple Test cases in Bash script

--- run_chrome.sh
+++ run_chrome.sh
@@ -8,4 +8,4 @@ set -xe
 
 cd "newActivityOverview" && katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/Activity Overview detail Pop-up Other Section" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
 
-cd "newActivityOverview" && katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
+#cd "newActivityOverview" && katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
