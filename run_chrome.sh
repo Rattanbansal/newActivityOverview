1beee588ec65d005d06532d1ee2895aeb88f86ae
Rattan Bansal <rattan.intersoft@gmail.com>
2022-02-26 16:57:06 +0530

    cash file updated to run test cases

--- run_chrome.sh
+++ run_chrome.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-#docker run -t --rm -v /home/intersoft-admin/Rattan/Katalon/newActivityOverview:/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
+docker run -t --rm -v /home/intersoft-admin/Rattan/Katalon/newActivityOverview:/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
 
 #docker run -t --rm -v /root/project/newActivityOverview:/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
 
-cd "newActivityOverview" && katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/Activity Overview detail Pop-up Other Section" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
+#cd "newActivityOverview" && katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/Activity Overview detail Pop-up Other Section" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
 
 #cd "newActivityOverview" && katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/ActivityOverview-FiltersAndColumns" -apiKey="22937998-3f24-4da4-aedb-4d9bae4a896d" -executionProfile="Development"
