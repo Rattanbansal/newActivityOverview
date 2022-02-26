704436f1b8e6f215d88ab35b3420d5c92f1cc43f
Rattan Bansal <rattan.intersoft@gmail.com>
2022-02-26 17:35:26 +0530

    name test collection name

--- Test Suites/chromcollection.ts
+++ Test Suites/chromcollection.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>All Cases chrome</name>
+   <name>chromcollection</name>
    <tag></tag>
    <delayBetweenInstances>2</delayBetweenInstances>
    <executionMode>PARALLEL</executionMode>
